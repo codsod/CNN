f58e33d7e4f22dd8e3f848b3f1e0657c1ad2c9bd
codsod <3140323339@qq.com>
2026-05-07 14:45:00 +0800

    pool_stream

--- src/pool.h
+++ src/pool.h
@@ -20,37 +20,43 @@ class POOL{
 public:
     static constexpr int VEC = SPATIAL_VEC;
     static constexpr int CHUNKS = (DIM2 + VEC - 1) / VEC;
+    static constexpr int SEGMENTS = OUTDIM + 1;
+    static_assert(K == 40, "POOL rounding assumes K == 40");
+    static_assert(K == 2 * S, "POOL segment accumulation assumes K == 2*S");
 
     POOL(){};
 
     void do_pool_func(hls::stream<hls::vector<if_t, VEC> >& i_stream,hls::stream<hls::vector<of_t, DIM1 * OUTDIM>>& o_stream){
-        if_t in_buf[DIM1][DIM2];
-#pragma HLS ARRAY_PARTITION variable = in_buf complete dim = 2
-#pragma HLS RESET variable=in_buf off
-        
         n_loop:for(int n = 0 ; n < N ; n++){
             channels_loop:for(int chan = 0 ;chan < CHANNELS ; chan ++){
-                load_dim1_loop:for (int dim1 = 0; dim1 < DIM1; dim1++) {
+                hls::vector<of_t, DIM1 * OUTDIM> out;
+                #pragma HLS RESET variable=out off
+
+                dim1_loop:for (int dim1 = 0; dim1 < DIM1; dim1++) {
+                    uint16_t seg_sum[SEGMENTS];
+                    #pragma HLS ARRAY_PARTITION variable=seg_sum complete dim=1
+                    #pragma HLS RESET variable=seg_sum off
+
+                    init_seg_loop:for (int seg = 0; seg < SEGMENTS; seg++) {
+                        #pragma HLS UNROLL
+                        seg_sum[seg] = 0;
+                    }
+
                     load_chunks_loop:for (int ck = 0; ck < CHUNKS; ck++) {
                         hls::vector<if_t, VEC> in = i_stream.read();
                         load_elem_loop:for (int k = 0; k < VEC; k++) {
 #pragma HLS PIPELINE II=1
-#pragma HLS UNROLL factor=8
                             int t = ck * VEC + k;
-                            if (t < DIM2)
-                                in_buf[dim1][t] = in[k];
+                            int seg = t / S;
+                            if (t < DIM2 && seg < SEGMENTS)
+                                seg_sum[seg] += (uint16_t)in[k];
                         }
                     }
-                }
-                hls::vector<of_t, DIM1 * OUTDIM> out;
-                #pragma HLS RESET variable=out off
-                dim1_loop:for (int dim1 = 0; dim1 < DIM1; dim1++) {
+
                     outdim_loop:for (int outdim = 0 ;outdim < OUTDIM ;outdim++){
-                        uint16_t sum=0;
-                        k_loop:for(int k = 0; k < K ; k++){
-                            #pragma HLS UNROLL 
-                            sum += in_buf[dim1][outdim * S + k];
-                        }
+                        #pragma HLS PIPELINE II=1
+                        uint16_t sum = seg_sum[outdim] + seg_sum[outdim + 1];
+
                         // 取整方式须与量化 trace 一致。当前 ref（quant/acc0304.ipynb VerificationProbe）
                         // 导出 trace_pool1 时 PyTorch 量化 AvgPool 实际为截断，故用 sum/K。
                         // 若改用四舍五入与 pool1_meta（同 scale）对齐，可改为: (sum + K/2) / K
